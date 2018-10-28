3da5209bccb3e106250eca2f969847654a2d3ddb
ChenCyl <programming2@163.com>
2018-10-28 19:33:40 +0800

    add descriptions for pattern

--- HappyFarm/src/building/tile/TilePool.java
+++ HappyFarm/src/building/tile/TilePool.java
@@ -1,7 +1,15 @@
 package building.tile;
 
+/**
+ * @project: HappyFarm
+ * @description: 地砖库
+ * @designPattern: Flyweight
+ * @author: Chen Yulei
+ * @date: 2018-10-27
+ **/
 public class TilePool extends Tile {
 
+    // 地砖的样式（如红色碎花）作为key
     private String key;
 
     public TilePool(String key) {
