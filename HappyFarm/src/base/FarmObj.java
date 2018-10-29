6a461f9bc270c9d96cc5d0d76a0d0e38f2b7f53e
mikomikomoko <974328714@qq.com>
2018-10-29 16:45:19 +0800

    FarmObj中name重构为typename

--- HappyFarm/src/base/FarmObj.java
+++ HappyFarm/src/base/FarmObj.java
@@ -1,5 +1,13 @@
 package base;
 
 public class FarmObj {
-    protected String name;  //此处用name还是type有待商榷
+    protected String typename;  //此处用name还是type有待商榷
+
+    public String getTypename() {
+        return typename;
+    }
+
+    public void setTypename(String typename) {
+        this.typename = typename;
+    }
 }
