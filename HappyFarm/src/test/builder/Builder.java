44cd45970219aec02d6888bbc8a3c24cf6849800
Goasttime <707568378@qq.com>
2018-10-31 01:39:35 +0900

    add Builder Test

---
+++ HappyFarm/src/test/builder/Builder.java
@@ -0,0 +1,30 @@
+package test.builder;
+
+import plant.Apple;
+import plant.Cabbage;
+import plant.Rice;
+import plant.Wheat;
+import store.Combo.Combo;
+import store.Combo.ComboBuilderA;
+import store.Combo.ComboBuilderB;
+import store.Combo.Director;
+
+public class Builder {
+    public static void main(String args[]){
+        new Apple();
+        new Wheat();
+        new Cabbage();
+        new Rice();
+        Director director = new Director();
+        store.Combo.Builder builderA = new ComboBuilderA();
+        store.Combo.Builder builderB = new ComboBuilderB();
+
+        director.ConstructBuild(builderA);
+        Combo comboA = builderA.getCombo();
+        comboA.showCombo();
+
+        director.ConstructBuild(builderB);
+        Combo comboB = builderB.getCombo();
+        comboB.showCombo();
+    }
+}
