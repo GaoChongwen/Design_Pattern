eb6929337b746eef6a4a5ecb2556d8458bd6c958
mikomikomoko <974328714@qq.com>
2018-10-30 17:14:23 +0800

    set animal price

--- HappyFarm/src/animal/Pig.java
+++ HappyFarm/src/animal/Pig.java
@@ -1,6 +1,12 @@
 package animal;
 
 import base.animal.Animal;
+import utils.Context;
 
 public class Pig extends Animal {
+    public Pig(){
+        super();
+        setStockPrice(Context.getInstance().pig_stockPrice);
+        setSalePrice(Context.getInstance().pig_stockPrice);
+    }
 }
--- HappyFarm/src/utils/Context.java
+++ HappyFarm/src/utils/Context.java
@@ -13,6 +13,14 @@ public class Context {
         this.rice_salePrice = 230;
         this.wheat_stockPrice = 20;
         this.wheat_salePrice = 170;
+
+        //initial price of animals
+        this.chicken_stockPrice=0;
+        this.chicken_salePrice=0;
+        this.cow_stockPrice=0;
+        this.cow_salePrice=0;
+        this.pig_stockPrice=0;
+        this.pig_salePrice=0;
     }
 
     // price of plants
@@ -24,6 +32,14 @@ public class Context {
     public static int rice_salePrice;
     public static int wheat_stockPrice;
     public static int wheat_salePrice;
+
+    //price of animals
+    public static int chicken_stockPrice;
+    public static int chicken_salePrice;
+    public static int cow_stockPrice;
+    public static int cow_salePrice;
+    public static int pig_stockPrice;
+    public static int pig_salePrice;
 
     public static Context getInstance() {
         return singleton;
