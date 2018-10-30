28cfe1836c06cecbf68890e0b69a6f1ef3698bd7
ChenCyl <programming2@163.com>
2018-10-31 00:09:52 +0800

    add method to shed

---
+++ HappyFarm/src/sampleProgram/Main.java
@@ -0,0 +1,45 @@
+package sampleProgram;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Welcome to Happy Farm!");
+	    System.out.println("欢迎来到快乐农场，农场中有五个元素：农场主的房子、种植园、牲畜园、商店和雇员。");
+        System.out.println("种植园：有4块不同种类的土地，分别对应不同种类的植物");
+
+	    System.out.println("1. Abstract Factory");
+	    System.out.println("2. Builder");
+        System.out.println("3. Factory Method");
+        System.out.println("4. Prototype");
+        System.out.println("5. Singleton");
+        System.out.println("6. Proxy");
+        System.out.println("7. Flyweight");
+        System.out.println("8. Decorator");
+        System.out.println("9. Composite");
+        System.out.println("10. Adaptor");
+        System.out.println("11. Bridge");
+        System.out.println("12. Filter");
+        System.out.println("13. Facade");
+        System.out.println("14. Strategy");
+        System.out.println("15. Template Method");
+        System.out.println("16. Observer");
+        System.out.println("17. Iterator");
+        System.out.println("18. Chain of Responsibility");
+        System.out.println("19. Command");
+        System.out.println("20. Memento");
+        System.out.println("21. State");
+        System.out.println("22. Visitor");
+        System.out.println("23. Mediator");
+        System.out.println("24. Interpretor");
+        System.out.println("25. Null Object");
+        System.out.println("26. Composite Entity");
+        System.out.println("27. Specification");
+        System.out.println("28. Double Checked Locking");
+
+//	    while
+//	    System.out.println("");
+
+	    // 自己写自己的使用场景
+
+    }
+}
