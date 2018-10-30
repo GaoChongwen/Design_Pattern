a921db07fba826983859a959fb0edae9223d8cae
QI Batu <15750659557@163.com>
2018-10-30 12:08:19 +0900

    :

--- HappyFarm/src/store/Store.java
+++ HappyFarm/src/store/Store.java
@@ -1,6 +1,9 @@
 package store;
 
 import base.Item;
+import plant.Apple;
+import singleton.Farm;
+import singleton.MessageBoard;
 
 
 import java.util.ArrayList;
@@ -8,8 +11,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * 商店类，接口包括购买商品，添加商品，删除商品
+ */
 public class Store{
+    private volatile static Store singleton = new Store();
 
+    public static Store getInstance() { return singleton; }
+
+    private Store(){ }
    private Map<String,Integer> Commodities = new HashMap<String, Integer>();
     //添加商品,指定商品名和添加数量
    public void addCommity(String name , Integer number){
@@ -24,7 +34,9 @@ public class Store{
    public List<Item> getCommity(String name, Integer number){
        List<Item> result = new ArrayList<Item>();
        if(Commodities.containsKey(name)){
+           for(Integer i=0 ; i<number ; i++){
 
+           }
        }
        return result;
    }
--- HappyFarm/src/utils/test/Store.java
+++ HappyFarm/src/utils/test/Store.java
@@ -1,4 +1,26 @@
 package utils.test;
 
+
+import utils.Enum.PlantType;
+
 public class Store {
+    public static void main(String args[]){
+   final String num1 = "q";
+   final String num6 = "w";
+   final String num8 = "e";
+   PlantType e1 = Enum.valueOf(PlantType.class, num1);
+   PlantType e6 = Enum.valueOf(PlantType.class, num6);
+   PlantType e8 = Enum.valueOf(PlantType.class, num8);
+   System.out.println("字符转枚举： " + e1+"\t"+e6.toString()+"\t"+e8.name());
+   if(e1.compareTo(e6)>0){
+        System.out.println(e1.name()+">"+e6.name());
+     }else {
+       System.out.println(e1.name()+"<"+e6.name());
+   }
+   if(e6.compareTo(e8)>0){
+     System.out.println(e6.name()+">"+e8.name());
+     }else{
+     System.out.println(e6.name()+"<"+e8.name());
+ }
+}
 }
