645bd282b8598a3419e77fab194ece8a1f6271cf
mikomikomoko <974328714@qq.com>
2018-10-30 21:10:45 +0800

    state test

--- HappyFarm/src/base/animal/GrowingState.java
+++ HappyFarm/src/base/animal/GrowingState.java
@@ -14,13 +14,16 @@ public class GrowingState implements AnimalState{
         int num=ra.nextInt(100)+1;
         //百分之70的概率成长为成熟期
         if(num<=20) {
+            System.out.println(animal.getName()+"没有成长");
             animal.setState(animal.growingState);
         }
         else if(num>20&&num<90){
+            System.out.println(animal.getName()+"成长为了成熟期");
             animal.setState(animal.matureState);
             animal.isMature=true;
         }
         else{
+            System.out.println(animal.getName()+"病死了");
             animal.setState(animal.deadState);
             animal.shouldRemove=true;
         }
@@ -31,6 +34,7 @@ public class GrowingState implements AnimalState{
         Random ra=new Random();
         int num=ra.nextInt(100)+1;
         if(num>70){
+            System.out.println(animal.getName()+"成长为了成熟期");
             animal.setState(animal.matureState);
             animal.isMature=true;
         }
---
+++ HappyFarm/src/utils/test/State.java
@@ -0,0 +1,36 @@
+package utils.test;
+
+import base.animal.Animal;
+import factory.AbstractFactory;
+import factory.AnimalFactory;
+
+import java.util.Random;
+
+public class State {
+    public static void main(String args[]){
+        Animal[] animals=new Animal[10];
+        AbstractFactory animalFactory=new AnimalFactory();
+        for(int i=0; i<10; ++i) {
+            Random ra = new Random();
+            int num = ra.nextInt(2) + 1;
+            if(num==1){
+                animals[i]=((AnimalFactory) animalFactory).createAnimal("chicken");
+            }
+            else if(num==2){
+                animals[i]=((AnimalFactory) animalFactory).createAnimal("cow");
+            }
+            else if(num==3){
+                animals[i]=((AnimalFactory) animalFactory).createAnimal("pig");
+            }
+            else{
+                System.out.println("system error");
+            }
+        }
+
+        for(int i=0; i<10; ++i){
+            if(!animals[i].shouldRemove){
+                animals[i].grow();
+            }
+        }
+    }
+}
