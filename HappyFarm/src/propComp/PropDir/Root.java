f80b7a6eaade3b1bc98a567b12aefaaf7fd8ac47
Logan1004 <loganluo1004@126.com>
2018-10-31 13:21:09 +0800

    Update root
    root info will update at the beginning of the new circle

--- HappyFarm/src/propComp/PropDir/Root.java
+++ HappyFarm/src/propComp/PropDir/Root.java
@@ -1,5 +1,10 @@
 package propComp.PropDir;
 
+import utils.clock.Clock;
+
+import java.util.Observable;
+import java.util.Observer;
+
 /**
  * @Design-Pattern: Singleton Pattern
  * @description: 生成实例 方便使用工具箱
@@ -7,7 +12,7 @@ package propComp.PropDir;
  * @author jihao luo
  *
  */
-public class Root {
+public class Root implements Observer{
     private static Root root = new Root();
     public Node propDir = new Node("Item");
     private Root(){}
@@ -17,4 +22,8 @@ public class Root {
         return root;
     }
 
+    @Override
+    public void update(Observable o, Object arg) {
+        Prop.CheckAndRemove(propDir);
+    }
 }
