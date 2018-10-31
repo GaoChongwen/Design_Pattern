8d7b16a927064e8bd4a1b14403a409452cc9f0af
Goasttime <707568378@qq.com>
2018-10-31 20:09:46 +0900

    update Store for function Show()

--- HappyFarm/src/test/Store.java
+++ HappyFarm/src/test/Store.java
@@ -5,6 +5,7 @@ import animal.Cow;
 import base.plant.Plant;
 import building.shed.CowShed;
 import factory.PlantFactory;
+import person.Employee;
 import plant.Apple;
 import plant.Cabbage;
 import plant.Rice;
@@ -13,6 +14,9 @@ import propComp.PropDir.Prop;
 import singleton.Farm;
 import singleton.Repository;
 import utils.Enum.PlantType;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 商店测试，目前植物可以完全支持，动物可能有问题，其他未添加
@@ -25,15 +29,20 @@ public class Store {
         new Cabbage();
         new Rice();
         Prop prop = new Prop();
+
         /**
-         * 测试购买，暂只支持购买animal和plant
+         * 测试购买，暂只支持买卖animal,plant，购买adaptor
          */
 
         store.Store.getInstance().buyCommity("wheat",2);
         System.out.println("Does the warehouse contain wheat?，true or false ");
         System.out.println(Repository.getInstance().getPlantNum("wheat"));
         store.Store.getInstance().buyCommity("cow",2);
-        System.out.println("Does the warehouse contain cow?，true or false ");
+        System.out.println("Does the cowshed contain cow?，true or false ");
         System.out.println(!CowShed.getInstance().isEmpty());
+        store.Store.getInstance().buyCommity("appleField",2);
+        System.out.println("Does user have appleField?，true or false ");
+        System.out.println("");
+
  }
 }
