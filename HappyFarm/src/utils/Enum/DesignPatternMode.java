b83b5dcc942512701d7463cdda7ef96e8b863fdd
gaosaroma <gaosaroma@gmail.com>
2018-10-31 12:53:33 +0800

    update the designPatternMode

--- HappyFarm/src/utils/Enum/DesignPatternMode.java
+++ HappyFarm/src/utils/Enum/DesignPatternMode.java
@@ -3,4 +3,14 @@ package utils.Enum;
 public enum DesignPatternMode {
     AdaptorPattern, // 适配器模式
     FactoryPattern, // 工厂模式
+    AbstractFactoryPattern, // 抽象工厂模式
+    FacadePattern, // 外观模式
+    TemplateMethodPattern, // 模板模式
+    BridgePattern, //桥接模式
+    MediatorPattern, //中介者模式
+    ObserverPattern, //观察者模式
+    BuilderPattern, //
+    ChainofResponsibilityPattern, // 责任链模式
+    CommandPattern, // 命令模式
+    CompositePattern, // 组合模式
 }
