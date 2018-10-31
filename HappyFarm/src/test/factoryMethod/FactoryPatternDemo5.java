87794bca16e49365c270bb964b3caa7f49a47491
Logan1004 <loganluo1004@126.com>
2018-10-31 15:37:50 +0800

    Update
    Update Prototype test &  Factory pattern

---
+++ HappyFarm/src/test/factoryMethod/FactoryPatternDemo5.java
@@ -0,0 +1,52 @@
+package test.factoryMethod;
+
+import factory.CouponFactor;
+import utils.Enum.PlantType;
+import utils.Money.Money;
+
+/**
+ * @project: HappyFarm
+ * @scene: 在商店购买兑换券后，生成兑换券
+ * @designPattern: Factory
+ * @author: Jihao Luo
+ * @date: 2018-10-30
+ **/
+
+public class FactoryPatternDemo5 {
+    public static void main(String args[]){
+
+        System.out.println("==========Factory_DEMO========");
+        System.out.println("Scene: After buying certain coupons in the store, we need to create coupons.\n" +
+                "In the program ,ChainOfResponsibility Pattern is used to vertify the kind of the coupon；");
+        System.out.println("===================================================");
+        System.out.println("=============Then we create the instance===========");
+        CouponFactor.getInstance().CouponInitial();//Initial !!!!!!
+
+
+        CouponFactor.getInstance().add(PlantType.apple.toString());
+        System.out.println("Factory:CouponFactor:addCoupon:Apple Coupon Created");
+        CouponFactor.getInstance().add(PlantType.wheat.toString());
+        System.out.println("Factory:CouponFactor:addCoupon:Wheat Coupon Created");
+        CouponFactor.getInstance().add(PlantType.rice.toString());
+        System.out.println("Factory:CouponFactor:addCoupon:Rice Coupon Created");
+        CouponFactor.getInstance().add(PlantType.cabbage.toString());
+        System.out.println("Factory:CouponFactor:addCoupon:Cabbage Coupon Created");
+        CouponFactor.getInstance().add("super");
+        System.out.println("Factory:CouponFactor:addCoupon:Super Coupon Created");
+        System.out.println("==========Now we try to use certain coupons========");
+        System.out.println("==========In this demo we have money 200===========");
+        //System.out.println(Money.getInstance().couponList.size());
+        System.out.println("=========And the coupons you created above=========");
+        System.out.println("=========if true , we buy items successfully=======");
+        System.out.println("=2 apples=");
+        System.out.println(Money.getInstance().buy("Apple",2,100));
+        System.out.println("=then 2 wheat=");
+        System.out.println(Money.getInstance().buy("Wheat",2,100));
+        System.out.println("=1 more wheat=");
+        System.out.println(Money.getInstance().buy("Wheat",1,100));
+
+        //System.out.println(Money.getInstance().couponList.size());
+
+    }
+
+}
